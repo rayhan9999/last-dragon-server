a18bb934fa02f2f74b909eb859b808c426b95b06
rayhan9999 <rayhanchowdhuri2023@gmail.com>
2023-05-03 21:48:21 +0600

    server-side

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.port || 5000;
+
+
+
+const categories = require('./data/categories.json');
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Last Dragon is running port ')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.listen(port, () => {
+    console.log(`Dragon api is running  on port ${port}`)
+})
